50b172ca4d90a18b8e2217ecc3b7641e40e6042c
Cherfo <82979766+FernandoV17@users.noreply.github.com>
2025-05-18 21:37:59 -0600

    Dev Init

---
+++ Firmware/PIA_MICRO/PIA_MICRO/DISPLAY_7SEG_MUX/DISPLAY_7SEG_MUX.h
@@ -0,0 +1,32 @@
+/*
+ * DISPLAY_7SEG_MUX.h
+ *
+ * Created: 17/05/2025 03:33:11 p. m.
+ *  Author: fervi
+ */ 
+
+
+#ifndef DISPLAY_7SEG_MUX_H_
+#define DISPLAY_7SEG_MUX_H_
+
+#include <avr/io.h>
+
+// Definiciones de pines
+#define display_7SEG_DDRX   DDRD
+#define display_7SEG_PORTX  PORTD
+
+#define display_mux_DDRX    DDRC
+#define display_mux_PORTX   PORTC
+
+// Pines de control de dígitos (ajustar según conexión física)
+#define DIG1 PC0
+#define DIG2 PC1
+#define DIG3 PC2
+#define DIG4 PC3
+
+// Prototipos de funciones
+void init_display_mux(void);
+void timer1_init(void);
+void update_display(uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4);
+
+#endif /* DISPLAY_7SEG_MUX_H_ */
---
+++ Firmware/PIA_MICRO/PIA_MICRO/main.c
@@ -0,0 +1,18 @@
+/*
+ * PIA_MICRO.c
+ *
+ * Created: 17/05/2025 03:24:13 p. m.
+ * Author : fervi
+ */ 
+
+#include <avr/io.h>
+#include "DISPLAY_7SEG_MUX/DISPLAY_7SEG_MUX.h"
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
